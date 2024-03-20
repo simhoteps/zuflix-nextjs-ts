59ffa93f727cf6a1735b7554afa1dd2320e8c1b2
Simhotep <78691738+simhoteps@users.noreply.github.com>
2024-03-20 10:12:14 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+    };
+  },
+};
 
 export default nextConfig;
