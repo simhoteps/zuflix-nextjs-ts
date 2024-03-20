3726921065008d9c0f6bf629cd07af20b6ef576d
Simhotep <78691738+simhoteps@users.noreply.github.com>
2024-03-20 10:39:07 +0300

    retry

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/zuhalhayat/Documents/GitHub/zuflix-nextjs-ts/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"MsMDQ7D4yxpDj2ABwbbfWdlHngI3I+S0TDX0H5Fc2gs=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"wDJn6DR7E4GOJKE7LOIl9w9RIPWvfAnnNH70lAx1mdc=\"\n}"
